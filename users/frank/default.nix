17f105058bc13750d2fe55eee865a37f357df35d
Frank Beutelschiess <frank@thesym.site>
2022-01-31 02:21:39 +0100

    add user frank

---
+++ users/frank/default.nix
@@ -0,0 +1,16 @@
+{ lib, pkgs, ... }:
+{
+  users.users.frank = {
+    uid            = 1428;
+    hashedPassword = "$6$zFsull/7$I9f/Vorw7AU8uPnJcuIZdhVDh3ogCd6h/D7wsYcA46G1IF8w3Yn79hHU6YiHUKNH3RFTHxfnJwmYTU3TLsIMW/";
+    description    = "Frank Beutelschiess";
+    isNormalUser   = true;
+    extraGroups    = [
+      "wheel"
+      "docker"
+    ];
+    ### TODO if enabled docker : docker
+  };
+}
+
+### `mkpasswd -m sha-512 PASSWORD`
