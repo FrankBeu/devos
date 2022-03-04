57d151ad4291906fbaa3cb34aba929fd9201963d
Frank Beutelschiess <frank@thesym.site>
2022-03-04 23:25:37 +0100

    tests: recursive collectTests

--- lib/testing/default.nix
+++ lib/testing/default.nix
@@ -30,19 +30,34 @@ let
   nixosTesting.makeTest (maybeCallTest pkgs (maybeImport test));
 
 
+  /* importTestIfAttributeIsPath walks all attributes (DFS) and imports the test if a path is found */
+  importTestIfAttributeIsPath =
+    setOfPaths: builtins.mapAttrs (name: value: if builtins.isPath value
+                                                then import value { inherit self mkTest;}
+                                                else importTestIfAttributeIsPath value ) setOfPaths;
+
+  /* eliminateNestedDuplicatedTestname walks all attributes (DFS) and removes the first of two identical consecutive attributeNames
+  ATTRIBUTE.TESTNAME.TESTNAME -> ATTRIBUTE.TESTNAME */
+  eliminateNestedDuplicatedTestnames =
+    test: builtins.mapAttrs (name: value: if name == builtins.elemAt (builtins.attrNames value) 0
+                                            then value.${builtins.elemAt (builtins.attrNames value) 0}
+                                            else eliminateNestedDuplicatedTestnames value) test;
+
+  ### TODO write tests after realignment with digga
   /* collectTests uses digga..rakedLeaves to collect all tests from the given path.
   In order to be able to build one of the collected test with the shortest name possible (`nix build .#tests.testName`) some reshaping has to be done.
   1. All paths are collected.
   2. All paths are imported with the required attributes.
-  3. Eliminate nested, duplicated attributeNames:
-      Because the automatically (in the testfile) created testname (based on the test-dirname)
-      is identical to the attributeName created by rakeLeaves (also based on the test-dirname) -
-      one can be eliminated. */
+  3. Eliminate nested, duplicated testNames:
+  Because the automatically created testName (in the testfile based on the test-dirname)
+  is identical to the testName created by rakeLeaves (also based on the test-dirname) -
+  one of them can be eliminated. */
   collectTests = directoryPath:
   let
-    rakedTestPaths = self.inputs.digga.lib.rakeLeaves directoryPath;                                      ### rakedTestPaths = { dirname1 = path1;  dirname2 = path2; ... };
-    tests          = builtins.mapAttrs (name: path: import path { inherit self mkTest;} ) rakedTestPaths; ### tests          = { dirname1  = { dirname1 = { testAttr1 } }; dirname2 ... } `nix build .#tests.testName.testName`
-  in builtins.mapAttrs (name: value: value.${builtins.elemAt (builtins.attrNames value) 0} ) tests;       ### collectTests   = { dirname1 = { testAttr1 }; dirname2 = { testAttr2 }; .. } `nix build .#tests.testName`
+    rakedTestPaths    = self.inputs.digga.lib.rakeLeaves   directoryPath;  ### rakedTestPaths = { dirname1 = path1; dirname2 = path2; ... };
+    setOfTests        = importTestIfAttributeIsPath        rakedTestPaths; ### tests          = { dirname1 = { dirname1 = { testAttr1 } }; dirname2 ... }       `nix build .#tests.testName.testName`
+    allCollectedTests = eliminateNestedDuplicatedTestnames setOfTests;     ### collectTests   = { dirname1 = { testAttr1 }; path.dirname2 = { testAttr2 }; .. } `nix build .#tests.testName`
+  in allCollectedTests;
 
 in
 { inherit collectTests; }
