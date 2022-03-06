17b079b8be8a4ff8cd706c1ab49531c1ed582523
Frank Beutelschiess <frank@thesym.site>
2022-03-06 13:06:10 +0100

    tests: add basic ranger test

---
+++ tests/nixos/profiles/filemanager/ranger/default.nix
@@ -0,0 +1,36 @@
+{ self, mkTest, ...}:
+let
+  ################################################### TEST-CONFIG
+  host = self.nixosConfigurations.NixOS;
+
+  # meta.timeout = 1800;
+  # meta.timeout = 50;
+
+  test = {
+    nodes = {
+      machine =
+        { suites, profiles, ... }: {
+          imports = [ profiles.filemanager.ranger ];
+        };
+    };
+
+    enableOcr  = true;
+    ############################################### TEST-CONFIG-END
+
+    testScript =
+      ''
+        start_all()
+        ${testScriptExternal}
+      '';
+
+      name = self.inputs.latest.lib.toUpper name;
+  };
+
+  name = with builtins; baseNameOf (toString ./.);
+
+  testScriptExternal = builtins.readFile ./testScript.py;
+
+in
+{
+  ${name} = mkTest host test;
+}
---
+++ tests/nixos/profiles/filemanager/ranger/testScript.py
@@ -0,0 +1,31 @@
+### TODO cleanUp
+### TODO interact
+### TODO check config
+machine.wait_for_unit("multi-user.target")
+
+with subtest("==================================================================================================== Check if RANGER is available"):
+    output = machine.succeed('whereis ranger')
+    machine.log(output)
+
+# machine.sleep(5)
+
+
+# machine.wait_for_unit("default.target")
+# if not "Linux" in machine.succeed("uname"):
+#     raise Exception("Wrong OS")
+
+
+# machine.succeed("ranger")
+# machine.execute("man ranger")
+# machine.get_screen_text 
+# man = machine.succeed("man ranger")
+# print(f"\n{man}\n")
+# machine.sleep(5)
+# machine.wait_until_succeeds("man ranger")
+# machine.execute("man ubectl")
+# output = machine.execute("ranger")[1]
+# machine.log(output)
+
+# machine.execute("pkill ranger")
+# machine.shell_interact
+# machine.screenshot("rangerm")
