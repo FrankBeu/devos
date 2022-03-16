e3ba7e75b059763de69f101ae4254b4b672f4458
Frank Beutelschiess <frank@thesym.site>
2022-03-16 17:19:01 +0100

    users: add authorizedKeys to root

--- users/root/default.nix
+++ users/root/default.nix
@@ -2,6 +2,10 @@
 # recommend using `hashedPassword`
 {
   ### TODO use sops
-  users.users.root.password = "root";
-  # users.users.root.password = "";
+  users.users.root = {
+    password = "root";
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM9/istIdrLtlvJXpU9nizspJ6knqO6uew9UDNy0kkOF (none)" ### frank via gpg, ssh-copy-id
+    ];
+  };
 }
