c0fbcc29a2be3cb1493bcd3591e258415cec941c
Frank Beutelschiess <frank@thesym.site>
2022-03-16 17:19:59 +0100

    modules: add description

--- nixos/modules/colorscheme/default.nix
+++ nixos/modules/colorscheme/default.nix
@@ -1,12 +1,14 @@
-###
-##
-# * colorscheme
-#
 { config, lib, ... }:
 {
   options.colorscheme = lib.mkOption {
-    type = lib.types.attrs;
-    default = { };
+    default     = { };
+    description = "A colorscheme which can be used throughout the system";
+    type        = lib.types.attrs;
+    ### TODO check the usage of XOR
+    # type        = lib.types.attrsOf;
+    # type        = lib.types.anything;
+    ### https://nixos.org/manual/nixos/stable/index.html#sec-option-types-basic
   };
+
   config._module.args.colorscheme = config.colorscheme;
 }
--- nixos/modules/variables/default.nix
+++ nixos/modules/variables/default.nix
@@ -6,8 +6,13 @@
 { config, lib, ... }:
 {
   options.variables = lib.mkOption {
-    type = lib.types.attrs;
-    default = { };
+    default     = { };
+    description = "A attributeSet which defines a set of globally usable variables";
+    type        = lib.types.attrs;
+    ### TODO check the usage of XOR
+    # type        = lib.types.attrsOf;
+    # type        = lib.types.anything;
+    ### https://nixos.org/manual/nixos/stable/index.html#sec-option-types-basic
   };
   config._module.args.variables = config.variables;
 }
