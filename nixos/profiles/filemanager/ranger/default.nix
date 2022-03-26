2c63ae0142f6ee5e914b4cad2336523a8d306944
Frank Beutelschiess <frank@thesym.site>
2022-03-26 20:10:25 +0100

    ranger: add global configuration

--- nixos/profiles/filemanager/ranger/default.nix
+++ nixos/profiles/filemanager/ranger/default.nix
@@ -5,9 +5,16 @@
       ranger
     ];
 
-    shellAliases =
-      {
-        r = "ranger";
-      };
+    shellAliases = {
+      r = "ranger";
+    };
+
+    etc."/ranger".source = ./ranger;
+
   };
+  ### TODO: use bat as highlighter
+  ### TODO: global configuration
+  ### mans '^FILES' ranger
+  ### try to use global configuration for apps used also by root: ranger, bat, vim, ripgrep, zsh??
+
 }
