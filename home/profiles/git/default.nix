04fec1c49f9c7ef9d8a60650fe9cc22d9fdd3c21
Frank Beutelschiess <frank@thesym.site>
2022-03-26 20:13:20 +0100

    reformat: nixos/profiles/{git,core}

--- home/profiles/git/default.nix
+++ home/profiles/git/default.nix
@@ -7,39 +7,39 @@
     };
 
     aliases = {
-      a = "add -p";
-      co = "checkout";
+      a   = "add -p";
+      bD  = "branch -D";
+      ba  = "branch -a";
+      bd  = "branch -d";
+      c   = "commit";
+      co  = "checkout";
       cob = "checkout -b";
-      f = "fetch -p";
-      c = "commit";
-      p = "push";
-      ba = "branch -a";
-      bd = "branch -d";
-      bD = "branch -D";
-      d = "diff";
-      dc = "diff --cached";
-      ds = "diff --staged";
-      r = "restore";
-      rs = "restore --staged";
-      st = "status -sb";
+      d   = "diff";
+      dc  = "diff --cached";
+      ds  = "diff --staged";
+      f   = "fetch -p";
+      i   = "init";
+      p   = "push";
+      r   = "restore";
+      ro  = "remote get-url origin";
+      rs  = "restore --staged";
+      st  = "status -sb";
 
-      # reset
+      ### reset
       soft = "reset --soft";
       hard = "reset --hard";
       s1ft = "soft HEAD~1";
       h1rd = "hard HEAD~1";
 
-      # logging
-      lg =
-        "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
-      plog =
-        "log --graph --pretty='format:%C(red)%d%C(reset) %C(yellow)%h%C(reset) %ar %C(green)%aN%C(reset) %s'";
-      tlog =
-        "log --stat --since='1 Day Ago' --graph --pretty=oneline --abbrev-commit --date=relative";
+      ### logging
+      lg   = "log --color --graph --pretty =format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
+      plog = "log --graph --pretty         ='format:%C(red)%d%C(reset) %C(yellow)%h%C(reset) %ar %C(green)%aN%C(reset) %s'";
+      tlog = "log --stat --since           ='1 Day Ago' --graph --pretty =oneline --abbrev-commit --date =relative";
       rank = "shortlog -sn --no-merges";
 
-      # delete merged branches
+      ### delete merged branches
       bdm = "!git branch --merged | grep -v '*' | xargs -n 1 git branch -d";
     };
+
   };
 }
--- nixos/profiles/core/default.nix
+++ nixos/profiles/core/default.nix
@@ -2,21 +2,21 @@
 let inherit (lib) fileContents;
 in
 {
-  # Sets nrdxp.cachix.org binary cache which just speeds up some builds
+  ### Sets nrdxp.cachix.org binary cache which just speeds up some builds
   imports = [ ../cachix ];
 
-  # For rage encryption, all hosts need a ssh key pair
+  ### For rage encryption, all hosts need a ssh key pair
   services.openssh = {
     enable = true;
     openFirewall = lib.mkDefault false;
   };
 
-  # This is just a representation of the nix default
+  ### This is just a representation of the nix default
   nix.systemFeatures = [ "nixos-test" "benchmark" "big-parallel" "kvm" ];
 
   environment = {
 
-    # Selection of sysadmin tools that can come in handy
+    ### Selection of sysadmin tools that can come in handy
     systemPackages = with pkgs; [
       binutils
       coreutils
@@ -42,8 +42,8 @@ in
       whois
     ];
 
-    # Starship is a fast and featureful shell prompt
-    # starship.toml has sane defaults that can be changed there
+    ### Starship is a fast and featureful shell prompt
+    ### starship.toml has sane defaults that can be changed there
     shellInit = ''
       export STARSHIP_CONFIG=${
         pkgs.writeText "starship.toml"
@@ -55,57 +55,61 @@ in
       let ifSudo = lib.mkIf config.security.sudo.enable;
       in
       {
-        # quick cd
-        ".." = "cd ..";
-        "..." = "cd ../..";
-        "...." = "cd ../../..";
+        ### quick cd
+        ".."    = "cd ..";
+        "..."   = "cd ../..";
+        "...."  = "cd ../../..";
         "....." = "cd ../../../..";
 
-        # git
+        ### git
         g = "git";
 
-        # grep
+        ### grep
         grep = "rg";
-        gi = "grep -i";
+        gi   = "grep -i";
 
-        # internet ip
+        ### internet ip
         myip = "dig +short myip.opendns.com @208.67.222.222 2>&1";
 
-        # nix
-        n = "nix";
-        np = "n profile";
-        ni = "np install";
-        nr = "np remove";
-        ns = "n search --no-update-lock-file";
-        nf = "n flake";
-        nepl = "n repl '<nixpkgs>'";
-        srch = "ns nixos";
-        orch = "ns override";
-        nrb = ifSudo "sudo nixos-rebuild";
-        mn = ''
+        ### nix
+        n    =             "nix";
+        np   =             "n  profile";
+        ni   =             "np install";
+        nr   =             "np remove";
+        ns   =             "n  search --no-update-lock-file";
+        nf   =             "n  flake";
+        nepl =             "n  repl '<nixpkgs>'";
+        srch =             "ns nixos";
+        orch =             "ns override";
+        nrb  = ifSudo "sudo nixos-rebuild";
+        mn   = ''
           manix "" | grep '^# ' | sed 's/^# \(.*\) (.*/\1/;s/ (.*//;s/^# //' | sk --preview="manix '{}'" | xargs manix
         '';
 
-        # fix nixos-option
+        ### fix nixos-option
         nixos-option = "nixos-option -I nixpkgs=${self}/lib/compat";
 
-        # sudo
-        s = ifSudo "sudo -E ";
+        ### sudo
+        s  = ifSudo "sudo -E ";
         si = ifSudo "sudo -i";
         se = ifSudo "sudoedit";
 
-        # top
+        ### top
         top = "btm";
 
-        # systemd
-        ctl = "systemctl";
-        stl = ifSudo "s systemctl";
-        utl = "systemctl --user";
-        ut = "systemctl --user start";
-        un = "systemctl --user stop";
-        up = ifSudo "s systemctl start";
-        dn = ifSudo "s systemctl stop";
-        jtl = "journalctl";
+        ### systemd
+        ctl   =          "systemctl";
+        stl   = ifSudo "s systemctl";
+        utl   =          "systemctl --user";
+        ut    =          "systemctl --user start";
+        un    =          "systemctl --user stop";
+        up    = ifSudo "s systemctl        start";
+        dn    = ifSudo "s systemctl        stop";
+
+        jtl   =          "journalctl";
+        jtlu  =          "journalctl    -u";
+        jtlb  =          "journalctl -b";
+        jtlbu =          "journalctl -b -u";
 
       };
   };
@@ -123,19 +127,18 @@ in
   };
 
   nix = {
-
-    # Improve nix store disk usage
+    ### Improve nix store disk usage
     autoOptimiseStore = true;
     gc.automatic = true;
     optimise.automatic = true;
 
-    # Prevents impurities in builds
+    ### Prevents impurities in builds
     useSandbox = true;
 
-    # give root and @wheel special privileges with nix
+    ### give root and @wheel special privileges with nix
     trustedUsers = [ "root" "@wheel" ];
 
-    # Generally useful nix option defaults
+    ### Generally useful nix option defaults
     extraOptions = ''
       min-free = 536870912
       keep-outputs = true
@@ -146,17 +149,17 @@ in
   };
 
   programs.bash = {
-    # Enable starship
+    ### Enable starship
     promptInit = ''
       eval "$(${pkgs.starship}/bin/starship init bash)"
     '';
-    # Enable direnv, a tool for managing shell environments
+    ### Enable direnv, a tool for managing shell environments
     interactiveShellInit = ''
       eval "$(${pkgs.direnv}/bin/direnv hook bash)"
     '';
   };
 
-  # Service that makes Out of Memory Killer more effective
+  ### Service that makes Out of Memory Killer more effective
   services.earlyoom.enable = true;
 
 }
