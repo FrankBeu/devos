355fafade19a0810961402f4179cc5adcd68c0c3
Frank Beutelschiess <frank@thesym.site>
2022-04-18 19:46:55 +0200

    alacritty: fix config-indentation

--- tests/nixos/profiles/alacritty/testScript.py
+++ tests/nixos/profiles/alacritty/testScript.py
@@ -2,6 +2,5 @@ machine.wait_for_unit("multi-user.target")
 
 
 with subtest("▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ nixos.profiles.alacritty"):
-
     output = machine.succeed('ls /run/current-system/sw/bin/')
     assert_contains(output, 'alacritty')
