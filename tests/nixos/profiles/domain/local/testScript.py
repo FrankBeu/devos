43bdef88553f244b72b468f2adcd4d264bb1a821
Frank Beutelschiess <fbeutelschiess@gmail.com>
2022-06-17 10:21:21 +0200

    tests: improve docDotLocal related tests

--- tests/nixos/profiles/domain/local/testScript.py
+++ tests/nixos/profiles/domain/local/testScript.py
@@ -1,32 +1,32 @@
 machine.wait_for_unit("multi-user.target")
 
-with subtest("▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ Check if *.local is available"):
+with subtest("▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ tests.nixos.profiles.domain.local::Check if *.local is available"):
     machine.wait_for_unit("coredns.service")
     output = machine.succeed('drill @127.0.0.1 local.')
     assert_contains(output, "local.	0	IN	A	127.0.0.1")
     # machine.log(output)
 
 
-with subtest("▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ Check if doc.local is available"):
+with subtest("▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ tests.nixos.profiles.domain.local::Check if doc.local is available"):
     machine.wait_for_unit("coredns.service")
     output = machine.succeed('drill doc.local.')
     assert_contains(output, "doc.local.	0	IN	A	127.0.0.1")
     # machine.log(output)
 
 
-with subtest("▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ Check if documentation is served at doc.local"):
+with subtest("▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ tests.nixos.profiles.domain.local::Check if documentation is served at doc.local"):
     # _, doc = machine.systemctl("status documentation.service --no-pager")
     # machine.log(doc)
 
-    output = machine.succeed('curl doc.local')
+    output = machine.wait_until_succeeds('curl doc.local')
     assert_contains(output, "<title>homepage - Docs</title>")
     # machine.log(output)
 
 
-with subtest("▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ Check if DOMAIN-DOCUMENTATION is available"):
+with subtest("▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ tests.nixos.profiles.domain.local::Check if DOMAIN-DOCUMENTATION is available"):
     # _, doc = machine.systemctl("status documentation.service --no-pager")
     # machine.log(doc)
 
-    output = machine.succeed('curl localhost:41503/system/domain/')
+    output = machine.wait_until_succeeds('curl localhost:41503/system/domain/')
     assert_contains(output, "<title>domain - Docs</title>")
     # machine.log(output)
--- tests/nixos/profiles/services/documentation/testScript.py
+++ tests/nixos/profiles/services/documentation/testScript.py
@@ -1,20 +1,17 @@
 machine.wait_for_unit("multi-user.target")
 
-with subtest("▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ Check if DOCUMENTATION-SERVICE is available"):
-    # _, jobs = machine.systemctl("list-units --no-pager")
-    # machine.log(jobs)
+# with subtest("▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ Check if DOCUMENTATION-SERVICE is available"):
+with subtest("▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ tests.nixos.profiles.services.documentation"):
+    machine.wait_for_unit("documentation.service")
 
-    # _, doc = machine.systemctl("status documentation.service --no-pager")
-    # machine.log(doc)
+    homepage = machine.wait_until_succeeds('curl localhost:41503')
+    assert_contains(homepage, "<title>homepage - Docs</title>")
 
-    output = machine.succeed('curl localhost:41503')
-    assert_contains(output, "<title>homepage - Docs</title>")
-
-    output = machine.succeed('curl localhost:41503/structural/doc/')
-    assert_contains(output, "<title>doc - Docs</title>")
+    doc = machine.wait_until_succeeds('curl localhost:41503/structural/doc/')
+    assert_contains(doc, "<title>doc - Docs</title>")
 
 
-# with subtest("▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ Check if COLORS are set according to theme"):
+# with subtest("▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ tests.nixos.profiles.services.documentation::Check if COLORS are set according to theme"):
 #     colors = machine.succeed("cat /etc/docLocal/themes/theme/src/colors.scss")
 #     machine.log(colors)
 #     machine.succeed("diff /etc/docLocal/themes/theme/src/colors.scss{,.TEST}")
