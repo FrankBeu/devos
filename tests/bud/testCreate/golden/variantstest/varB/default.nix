21e77edfda07759646f34f7d891644dc3ca6bc68
Frank Beutelschiess <fbeutelschiess@gmail.com>
2022-06-25 15:37:26 +0200

    tests: update bud.testCreate.variants

--- tests/bud/testCreate/golden/variantstest/varB/default.nix
+++ tests/bud/testCreate/golden/variantstest/varB/default.nix
@@ -3,9 +3,10 @@ let
   ### TODO variantSpecific settings
 
   name   = with builtins; baseNameOf (toString ./.);
-  shared = (import ../shared/test.nix { inherit self mkTest testHelpers name
-  ### TODO forward variantSpecific settings
-  ; });
+  shared = (import ../shared/test.nix {
+    inherit mkTest name self testHelpers
+    ### TODO forward variantSpecific settings
+    ;});
 in
 with shared;
 {
