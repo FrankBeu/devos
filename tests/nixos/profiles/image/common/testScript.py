05151c873f29049fe01598643f55b844e9304a64
Frank Beutelschiess <fbeutelschiess@gmail.com>
2022-07-15 23:16:37 +0200

    nixos: profiles.image.comman add img2pdf

--- tests/nixos/profiles/image/common/testScript.py
+++ tests/nixos/profiles/image/common/testScript.py
@@ -4,6 +4,7 @@ machine.wait_for_unit("multi-user.target")
 with subtest("▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ tests.nixos.profiles.image.common"):
     output = machine.succeed('ls /run/current-system/sw/bin/')
     assert_contains_line(output, 'riv')
-    machine.log(output)
+    assert_contains_line(output, 'img2pdf')
+
 
 ### TODO check if patch is applied (behavior)
