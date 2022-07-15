e6c5b2b05be45c930c202fbdba878a6f9a10029b
Frank Beutelschiess <fbeutelschiess@gmail.com>
2022-07-15 23:16:37 +0200

    nixos: profiles.image.comman add img2pdf

--- nixos/profiles/image/common/default.nix
+++ nixos/profiles/image/common/default.nix
@@ -4,6 +4,7 @@
     systemPackages = with pkgs; [
       riv                                           ### https://github.com/Davejkane/riv
       imagemagick
+      img2pdf
     ];
 
     shellAliases = {
