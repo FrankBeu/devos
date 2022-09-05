47a6d410d413e9046ba4e5efceeb966bc19244a2
Frank Beutelschiess <fbeutelschiess@gmail.com>
2022-09-05 12:57:28 +0200

    reformat: pkgs

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -4,21 +4,23 @@ final: prev:
 {
   ### KEEP SOURCES FIRST,
   ### SOURCES-OVERLAY FROM NVFETCHER
-  sources               =  prev.callPackage (import ./_sources/generated.nix)  {};
+  sources               =  prev.callPackage (import ./_sources/generated.nix)          {};
 
 
   ### THEN, CALL PACKAGES WITH `FINAL.CALLPACKAGE`
   ### LOCALLY DEFINED PACKAGES
-  i3statusBar           = final.callPackage         ./i3statusBar              {};
-  shellColorDefinitions = final.callPackage         ./shellColorDefinitions    {};
-  revealjs              = final.callPackage         ./revealjs                 {};
-  riv                   = final.callPackage         ./riv                      {};
+  i3statusBar           = final.callPackage         ./i3statusBar                      {};
+  shellColorDefinitions = final.callPackage         ./shellColorDefinitions            {};
+  revealjs              = final.callPackage         ./revealjs                         {};
+  riv                   = final.callPackage         ./riv                              {};
+
 
   ### k8s
   kubectl-cert-manager  = final.callPackage         ./k8s/kubectl/plugins/cert-manager {};
   kubectl-neat          = final.callPackage         ./k8s/kubectl/plugins/neat         {};
   crd2pulumi            = final.callPackage         ./k8s/pulumi/crd2pulumi            {};
   kube2pulumi           = final.callPackage         ./k8s/pulumi/kube2pulumi           {};
+
 
   ### TEMPORARILY
   starship              = final.callPackage         ./starship                         {};
